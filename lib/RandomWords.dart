385baa0f7b4807015269a3bd9e17793f0c6a3109
Dustin Robison <Dustinrobi@gmail.com>
2019-06-07 11:37:18 -0700

    Favorite items selectable

--- lib/RandomWords.dart
+++ lib/RandomWords.dart
@@ -1,53 +1,52 @@
 import 'package:english_words/english_words.dart';
-import 'package:english_words/english_words.dart' as prefix0;
 import 'package:flutter/cupertino.dart';
 import 'package:flutter/material.dart';
 
 class RandomWordsState extends State<RandomWords> {
+  final List<WordPair> _suggestions = <WordPair>[];
+  final TextStyle _biggerFont = const TextStyle(fontSize: 18.0);
+  final Set<WordPair> _saved = Set<WordPair>();
+
+  Widget _buildSuggestions() {
+    return ListView.builder(
+        padding: const EdgeInsets.all(16.0),
+        itemBuilder: /*1*/ (context, i) {
+          if (i.isOdd) return Divider();
+          /*2*/
+
+          final index = i ~/ 2; /*3*/
+          if (index >= _suggestions.length) {
+            _suggestions.addAll(generateWordPairs().take(10)); /*4*/
+          }
+          return _buildRow(_suggestions[index]);
+        });
+  }
+
+  Widget _buildRow(WordPair pair) {
+    final bool alreadySaved = _saved.contains(pair);
+    return ListTile(
+      title: Text(pair.asPascalCase, style: _biggerFont),
+      trailing: Icon(
+        alreadySaved ? Icons.favorite : Icons.favorite_border,
+        color: alreadySaved ? Colors.red : null,
+      ),
+      onTap: () {
+        setState(() {
+          if (alreadySaved) {
+            _saved.remove(pair);
+          } else {
+            _saved.add(pair);
+          }
+        });
+      },
+    );
+  }
+
   @override
   Widget build(BuildContext context) {
-    final _suggestions = <WordPair>[];
-    final _biggerFont = const TextStyle(fontSize: 18.0);
-    final wordPair = WordPair.random();
-
-    Widget _buildRow(WordPair pair) {
-      return ListTile(title: Text(pair.asPascalCase, style: _biggerFont));
-    }
-
-    Widget _buildSuggestions() {
-      return ListView.builder(
-          padding: const EdgeInsets.all(16.0),
-          itemBuilder: (context, i) {
-            if (i.isOdd) return Divider(
-              color: Colors.black,
-            );
-
-            final index = i ~/ 2;
-            if (index >= _suggestions.length) {
-              _suggestions.addAll(prefix0.generateWordPairs().take(10));
-            }
-            return _buildRow(_suggestions[index]);
-          });
-    }
-
     return Scaffold(
         appBar: AppBar(title: Text("Random Word Page!")),
-        body: _buildSuggestions()
-
-//      Column(children: [
-//        Center(
-//            child: Padding(
-//          padding: EdgeInsetsDirectional.only(top: 20),
-//          child: Text(
-//            "First: " + wordPair.asPascalCase,
-//            style: TextStyle(fontSize: 22),
-//          ),
-//        )),
-//        Container(
-//          child: _buildSuggestions(),
-//        )
-//      ]),
-        );
+        body: _buildSuggestions());
   }
 }
 
